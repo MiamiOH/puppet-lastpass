f4f346c51e10908e11be1b5d07d6f454c863163a
Dirk Tepe <tepeds@miamioh.edu>
2017-02-21 20:11:45 -0500

    Remove unused shell expectations from spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,8 @@
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
-require 'rspec/shell/expectations'
 include RspecPuppetFacts
 
 RSpec.configure do |c|
   c.formatter = :documentation
-  c.include Rspec::Shell::Expectations
 end
